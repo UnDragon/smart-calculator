1f44ae4f2713b61bd1771b492e3bd265bb25c613
UnDragon <31067730+UnDragon@users.noreply.github.com>
2018-03-11 22:49:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,35 @@
 class SmartCalculator { 
 constructor(initialValue) { 
-this.result = "" + initialValue; 
+this.result = initialValue; 
 } 
 
 add(number) { 
 this.result = this.result + " + " + number; 
-this.iteration = eval(this.result); 
 return this; 
 } 
 
 subtract(number) { 
 this.result=this.result + " - " + number; 
-this.iteration = eval(this.result); 
 return this; 
 } 
 
 multiply(number) { 
 this.result=this.result + " * " + number; 
-this.iteration = eval(this.result); 
 return this; 
 } 
 
 devide(number) { 
 this.result = this.result + " / " + number; 
-this.iteration = eval(this.result); 
 return this; 
 } 
 
 pow(number) { 
 this.result = this.result + " ** " + number; 
-this.iteration = eval(this.result); 
 return this; 
 } 
 
 valueOf() { 
-return this.iteration; 
+return eval(this.result); 
 } 
 } 
 
